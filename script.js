6f4b64202c5c16db320dbe78f14191e084708a71
Bubileone <bojan.djurich@gmail.com>
2025-05-29 13:58:54 +0200

    Releasing some DOM manipulating instructions

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 'use strict';
-console.log('Checkout new git functionality Checkout');
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
